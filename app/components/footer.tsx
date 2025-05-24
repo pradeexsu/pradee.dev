4856ae0f7146d4ef5564675a374fde0650ec1a10
pradeexsu <pradeexsu@gmail.com>
2025-05-24 15:29:16 +0530

    Refactor code structure for improved readability and maintainability

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,6 @@ import { footerLinks } from 'data/info';
 import AnchorLink from './link';
 
 export default function Footer() {
-
   return (
     <footer className="mb-16 animate-intro sticky bottom-0">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
