741537ff07550732dfcd57aee335d346447794c0
pradeexsu <pradeexsu@gmail.com>
2025-06-07 03:09:58 +0530

    fix: update className in RootLayout to ensure dark mode styling is applied correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx('  ', GeistSans.variable, GeistMono.variable)}
+      className={cx('dark', GeistSans.variable, GeistMono.variable)}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto dark:bg-black dark:text-white text-black bg-white">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
